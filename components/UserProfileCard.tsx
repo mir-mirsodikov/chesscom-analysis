e5e7a375877890b98ac62fd7329cae456bd3b30b
Mirmukhammad Mirsodikov <mirsodikov.mir@gmail.com>
2026-04-15 12:31:46 -0500

    Dont render image if no profile image

--- components/UserProfileCard.tsx
+++ components/UserProfileCard.tsx
@@ -6,7 +6,9 @@ export function UserProfileCard({ avatar, name, username, url }: UserInfo) {
   return (
     <div className="bg-slate-300 rounded-md p-4 md:w-2/3 lg:w-1/2 mx-4 my-10 md:m-auto flex flex-col md:flex-row md:my-10">
       <div className="flex justify-center">
-        <ProfileImage src={avatar} />
+        {avatar &&
+          <ProfileImage src={avatar} />
+        }
       </div>
       <div className="text-center md:text-left md:ml-4 flex flex-col justify-center">
         <div className="flex m-auto md:m-0">
